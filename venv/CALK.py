0d9d14bae68d30744affe61c7ab4fb36c06939e8
DariaAria21 <plot_ia42@student.itstep.org>
2021-07-04 14:52:27 +0300

    new

---
+++ venv/CALK.py
@@ -0,0 +1,13 @@
+x = int(input('x = '))
+y = int(input('y = '))
+smth = input('smth ')
+if smth == '+':
+    print(x + y)
+elif smth == '-':
+    print(x - y)
+elif smth == '*':
+    print(x * y)
+elif smth == '/':
+    print(x / y)
+else:
+    print('?')
